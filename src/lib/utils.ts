f8f2799854997d61ef04c120f59a8f4e512be226
rafaelnath <rafaelnathanael41@yahoo.com>
2025-05-23 00:37:10 +0700

    learned: cva, clsx, twMerge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/** Notes:
+ * clsx & twMerge is [...].join(" ") on steroid
+ * clsx: combine classNames safely (ignore falsy values) and intelligently (accepts: arrays/objects/combined)
+ * twMerge: resolve redundant or conflicting classNames
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
