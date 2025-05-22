89c4b82c3c44d9f9c569e72f0f911c1bb34c67eb
rafaelnath <rafaelnathanael41@yahoo.com>
2025-05-23 00:36:25 +0700

    added shadcn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Button from '@/components/ui/Button.vue';
 </script>
 
 <template>
   <main>
     <a class="text-gray-800">Hello fellas!</a>
+    <Button variant="default" class="asd">AAA</Button>
   </main>
 </template>
